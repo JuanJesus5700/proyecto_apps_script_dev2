25f3e2107f088f54dc8756c1b49167c771f132d2
JuanJesus5700 <juanucan921@gmail.com>
2025-02-26 16:50:21 -0600

    definiendo arquitectura inicial de react en apps script

---
+++ backend/config/env.js
@@ -0,0 +1,7 @@
+function env_() {
+    return {
+ID_DATABASE:"1JOqayMPFxGcvl5efaHqwBmyh-p3oTJhydcwuejcBVhk",
+SH_REGISTRO_USUARIOS: "registro usuarios",
+    };
+    
+}
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const doGet = () => HtmlService.createTemplateFromFile("frontend/Index")
     'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"'
 ) 
 .setTitle("proyecto_apps_scrip_dev")
-.setFaviconUrl("https://cdn-icons-png.flaticon.com/128/4615/4615513.png");
+.setFaviconUrl("https://cdn-icons-png.flaticon.com/128/164/164372.png");
 
 const include = (ruta) => HtmlService.createHtmlOutputFromFile(ruta).getContent();
+
+
